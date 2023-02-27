5004c1ff89b11030b5f3bbae4cd9bd1aaf8b0775
BuildTools <unconfigured@null.spigotmc.org>
2023-02-26 22:14:36 -0600

    Fixing idk

---
+++ src/main/java/net/dancodes/simplemoney/item/ModItems.java
@@ -0,0 +1,34 @@
+package net.dancodes.simplemoney.item;
+
+import net.dancodes.simplemoney.SimpleMoney;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item COPPER_COIN = registerItem("copper_coin",
+            new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(SimpleMoney.MOD_ID, name), item);
+    }
+
+    public static void addItemsToItemGroup() {
+        addToItemGroup(ItemGroups.TOOLS, COPPER_COIN);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+
+    public static void registerModItems() {
+        SimpleMoney.LOGGER.info("Registering mod items for " + SimpleMoney.MOD_ID);
+
+        addItemsToItemGroup();
+    }
+}
